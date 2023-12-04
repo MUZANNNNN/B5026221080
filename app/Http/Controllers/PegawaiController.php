9d652ac6ba0f0e68825c6bd3b9e78172621e85b5
MUZANNNNN <fauzanmuhammad0511@gmail.com>
2023-12-05 00:47:28 +0700

    Tugas pertemuan tanggal 27 Nop

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -73,7 +73,8 @@ class PegawaiController extends Controller
 	public function hapus($id)
 	{
 		// menghapus data pegawai berdasarkan id yang dipilih
-		DB::table('pegawai')->where('pegawai_id',$id)->delete();
+		DB::table('pegawai')->where('pegawai_id',$id)
+                            ->delete();
 
 		// alihkan halaman ke halaman pegawai
 		return redirect('/pegawai');
@@ -84,13 +85,22 @@ class PegawaiController extends Controller
 		// menangkap data pencarian
 		$cari = $request->cari;
 
-    		// mengambil data dari table pegawai sesuai pencarian data
+    	// mengambil data dari table pegawai sesuai pencarian data
 		$pegawai = DB::table('pegawai')
 		            ->where('pegawai_nama','like',"%".$cari."%")
 		            ->paginate();
 
-    		// mengirim data pegawai ke view index
+        // mengirim data pegawai ke view index
 		return view('index',['pegawai' => $pegawai, 'cari' => $cari]);
+    }
 
+    public function view($id){
+        // mengambil data pegawai berdasarkan id yang dipilih
+        $pegawai = DB::table('pegawai')
+                    ->where('pegawai_id', $id)
+                    ->get();
+
+        // passing data pegawai yang didapat ke view edit.blade.php
+        return view('view', ['pegawai' => $pegawai]);
 	}
 }
